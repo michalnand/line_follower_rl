bec3a899d993f2c1901f236dc8f04f372cbb3e48
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-04-04 11:59:23 +0200

    common

--- src/gym_linefollower/linefollower_env.py
+++ src/gym_linefollower/linefollower_env.py
@@ -43,21 +43,23 @@ class LineFollowerEnv(gym.Env):
             self.obs = ObservationFrames(width, height, frame_stacking)
             self.observation_space = spaces.Box(low=0, high=1.0, shape=(frame_stacking, height, width), dtype=numpy.float)
  
-        self.action_space = spaces.Discrete(8)
+        self.action_space = spaces.Discrete(4)
 
         self.actions = []
         
         self.actions.append([0.0, 0.0])
-        self.actions.append([0.5, 0.5])
+        self.actions.append([0.3, 0.3])
 
         self.actions.append([0.1, 0.0])
         self.actions.append([0.0, 0.1])
 
+        '''
         self.actions.append([0.3, 0.0])
         self.actions.append([0.0, 0.3])
 
         self.actions.append([0.3, 0.2])
         self.actions.append([0.2, 0.3])
+        '''
 
         self.pb_client = PybulletClient()
         self.reset()
