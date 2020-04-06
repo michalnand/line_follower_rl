859f96f6ae1fd2457a53eaadc214a88510ee0c2f
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-04-06 16:27:28 +0200

    a2c

--- src/models/a2c/src/config.py
+++ src/models/a2c/src/config.py
@@ -4,11 +4,11 @@ class Config():
         self.type           = "a2c"
         
         self.gamma          = 0.99
-        self.learning_rate  = 0.002
-
+        self.learning_rate  = 0.001
+ 
         self.entropy_beta   = 0.01
 
-        self.batch_size     = 64
+        self.batch_size     = 128
         
 
 
